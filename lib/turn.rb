4e136048f7950de02b828f04fe94a406d9b991b6
placid-tier-4550 <james.ardery@gmail.com>
2020-08-28 16:25:23 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,18 @@
 def display_board(board)
-  puts "#{board[0]}" "|" "#{board[1]}" "|" "#{board[2]}"
-  puts " ----------- "
-  puts "#{board[3]}" "|" "#{board[4]}" "|" "#{board[5]}"
-  puts " ----------- "
-  puts "#{board[6]}" "|" "#{board[7]}" "|" "#{board[8]}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def position_taken?(board, index)
+  board[index] == "O" || board[index] == "X"
+end
+
+def valid_move?(board, index)
+  !(position_taken?(board, index)) && index.between?(0, 8)
+end
+
+def input_to_index(user_input)
+  user_input.to_i(-1)
