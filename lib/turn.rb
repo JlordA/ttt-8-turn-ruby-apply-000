18a3efd9dc610ee81ac3b9471896641b8f0a5ba3
placid-tier-4550 <james.ardery@gmail.com>
2020-08-29 15:59:56 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,3 +21,13 @@ end
 def move(board, input_to_index, value = "X")
   board[input_to_index] = value
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input_to_index(user_input)
+  if board(position_taken) == valid_move
+    move
+  else
+    board(position_taken) != valid_move
+    puts "Invalid Move"
+end
