c049b263312dcfc4b9878a8e37dbf394734c29e4
placid-tier-4550 <james.ardery@gmail.com>
2020-08-29 18:21:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,8 +18,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, input_to_index, value = "X")
-  board[input_to_index] = value
+def move(board, index, value = "X")
+  board[index] = value
 end
 
 def turn(board)
